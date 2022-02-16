5822d17d631f46bf47899a5b3252a8154aafcca8
jeongmin.lee <jaruda88@gmail.com>
2022-02-16 16:08:09 +0900

    database.py update

--- utils/database.py
+++ utils/database.py
@@ -34,69 +34,74 @@ class DBHandler:
 
 
     def select(self, query):
-        try:
-            with self.connection() as db:
-                db.cursor().execute(query)
-                result = db.cursor().fetchall()        
-        except pymysql.err.MySQLError as ME:
-            return ME.args
-        except pymysql.err.DatabaseError as DE:
-            return DE.args
-        except pymysql.err.OperationalError as OE:
-            return OE.args         
-        except pymysql.err.IntegrityError as ITE:
-            return ITE.args
-        except pymysql.err.InternalError as IE:
-            return IE.args
-        except pymysql.err.ProgrammingError as PE:
-            return PE.args   
-        finally:
-            db.cursor().close()    
-            return result
+        db = self.connection()
+
+        if type(db) is tuple:
+            return db
+        else:
+            try:
+                with db.cursor() as cursor:
+                    cursor.execute(query)
+                    result = cursor.fetchall()        
+            except pymysql.err.MySQLError as ME:
+                return ME.args
+            except pymysql.err.DatabaseError as DE:
+                return DE.args
+            except pymysql.err.OperationalError as OE:
+                return OE.args         
+            except pymysql.err.IntegrityError as ITE:
+                return ITE.args
+            except pymysql.err.InternalError as IE:
+                return IE.args
+            except pymysql.err.ProgrammingError as PE:
+                return PE.args   
+            finally:
+                db.cursor().close()    
+                return result
 
 
-    def insert(self, query):
-        try:
-            with self.connection() as db:
-                db.cursor().execute(query)
-                db.cursor().commit()
-        except pymysql.err.MySQLError as ME:
-            return ME.args
-        except pymysql.err.DatabaseError as DE:
-            return DE.args
-        except pymysql.err.OperationalError as OE:
-            return OE.args         
-        except pymysql.err.IntegrityError as ITE:
-            return ITE.args
-        except pymysql.err.InternalError as IE:
-            return IE.args
-        except pymysql.err.ProgrammingError as PE:
-            return PE.args   
-        finally:
-            db.cursor().close()    
-            return ('success')        
+    # def insert(self, query):
+    #     try:
+    #         with self.connection() as db:
+    #             db.cursor().execute(query)
+    #             db.cursor().commit()
+    #     except pymysql.err.MySQLError as ME:
+    #         return ME.args
+    #     except pymysql.err.DatabaseError as DE:
+    #         return DE.args
+    #     except pymysql.err.OperationalError as OE:
+    #         return OE.args         
+    #     except pymysql.err.IntegrityError as ITE:
+    #         return ITE.args
+    #     except pymysql.err.InternalError as IE:
+    #         return IE.args
+    #     except pymysql.err.ProgrammingError as PE:
+    #         return PE.args   
+    #     finally:
+    #         db.cursor().close()    
+    #         return ('success')        
 
     
-    def update(self, query):
-        try:
-            with self.connection() as db:
-                db.cursor().execute(query)
-                db.cursor().commit()
-        except pymysql.err.MySQLError as ME:
-            return ME.args
-        except pymysql.err.DatabaseError as DE:
-            return DE.args
-        except pymysql.err.OperationalError as OE:
-            return OE.args         
-        except pymysql.err.IntegrityError as ITE:
-            return ITE.args
-        except pymysql.err.InternalError as IE:
-            return IE.args
-        except pymysql.err.ProgrammingError as PE:
-            return PE.args   
-        finally:
-            db.cursor().close()    
-            return ('success')       
+    # def update(self, query):
+    #     try:
+    #         with self.connection() as db:
+    #             db.cursor().execute(query)
+    #             db.cursor().commit()
+    #     except pymysql.err.MySQLError as ME:
+    #         return ME.args
+    #     except pymysql.err.DatabaseError as DE:
+    #         return DE.args
+    #     except pymysql.err.OperationalError as OE:
+    #         return OE.args         
+    #     except pymysql.err.IntegrityError as ITE:
+    #         return ITE.args
+    #     except pymysql.err.InternalError as IE:
+    #         return IE.args
+    #     except pymysql.err.ProgrammingError as PE:
+    #         return PE.args   
+    #     finally:
+    #         db.cursor().close()    
+    #         return ('success')       
 
 
     def session(self, query):
