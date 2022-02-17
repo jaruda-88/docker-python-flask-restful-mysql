a8c50a13c1e8485cc263cde3be7475b1421a83fa
jeongmin.lee <jaruda88@gmail.com>
2022-02-17 17:12:21 +0900

    blueprint test

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 
-from flask import Flask, jsonify, request
-import utils.database
+from flask import Flask
+from .api import blueprint
 
 app = Flask(__name__)
+app.register_blueprint(blueprint)
 
-db_handler = utils.database.DBHandler()
+#db_handler = utils.database.DBHandler()
 
-@app.route('/', methods=['GET'])
-def vist():
-    if request.method == "GET":
-        _flag, data = db_handler.executer('''SELECT * FROM users;''')
-        return jsonify({"dd":data})
+# @app.route('/', methods=['GET'])
+# def vist():
+#     if request.method == "GET":
+#         _flag, data = db_handler.executer('''SELECT * FROM users;''')
+#         return jsonify({"dd":data})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5040', debug=True)
