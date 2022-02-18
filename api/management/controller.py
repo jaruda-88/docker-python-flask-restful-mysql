9097a46af4ac2d95184bb6c348101d007c77bf05
jeongmin.lee <jaruda88@gmail.com>
2022-02-18 16:35:04 +0900

    update

--- api/management/controller.py
+++ api/management/controller.py
@@ -12,17 +12,18 @@ db = utils.database.DBHandler()
 
 @api.route("/sample1")
 class Sample1Resource(Resource):
-    @api.marshal_with(ManagementDto.SampleResponse)
     def get(self):
         """ get sample1 """
-        return "sample"
+        _flag, records = db.executer(query='''SELECT * FROM users;''')
+        return records
 
 
 @api.route("/sample2/<input>")
 class Sample2Resource(Resource):
     @api.marshal_with(ManagementDto.SampleResponse, envelope="result")
     @api.doc(params={'input' : 'sample input'})
+    @api.response(200, 'Success')
+    @api.response(400, 'Bad Request')
     def get(self, input):
         """ get sample2 """
-        _flag, records = db.executer(query='''SELECT * FROM users;''')
-        return records
+        return input
