bf7cb3e24abc7c75f2736e92e66021cca30bcde9
jeongmin.lee <jaruda88@gmail.com>
2022-02-18 10:40:07 +0900

    api test

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,10 @@
 from flask import Blueprint
-from flask_restx import Api, Namespace
+from flask_restx import Api
 
 blueprint = Blueprint("api", __name__, url_prefix="/api")
 
 api = Api(blueprint, version='1.0', title='rest api', description="my study")
 
-management_ns = Namespace("management", description="api")
+management_ns = api.namespace("management", description="api", path="/management")
 
-api.add_namespace(management_ns, path="/management")
 
