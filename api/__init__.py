a7aa792d1a447acd69a0408aa07f17534dd32c3f
jeongmin.lee <jaruda88@gmail.com>
2022-02-18 10:52:58 +0900

    api test

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,17 @@
 from flask import Blueprint
-from flask_restx import Api
+from flask_restx import Api, Resource
 
 blueprint = Blueprint("api", __name__, url_prefix="/api")
 
 api = Api(blueprint, version='1.0', title='rest api', description="my study")
 
-management_ns = api.namespace("management", description="api", path="/management")
+management_ns = api.namespace("management", description="api")
+
+
+@management_ns.route("/")
+class TestResource(Resource):
+    def get(self):
+        """ ddddd """
+        pass
 
 
--- api/management/restx.py
+++
@@ -1,9 +0,0 @@
-from api import management_ns
-from flask_restx import Resource
-
-
-@management_ns.route("/test")
-class TestResource(Resource):
-    def get(self):
-        """ ddddd """
-        pass
