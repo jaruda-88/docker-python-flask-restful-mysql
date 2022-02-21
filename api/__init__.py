0507e0cfe91bce730c66abbf358fc6805818cb3e
jeongmin.lee <jaruda88@gmail.com>
2022-02-21 16:50:52 +0900

    swagger v1 delete

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,6 @@
 from flask import Blueprint
-from flask_restx import Api
-from api.management.controller import api as management_ns
 
 blueprint = Blueprint("api", __name__, url_prefix="/api")
-
-api = Api(blueprint, version='1.0', title='rest api', description="my study")
-
-api.add_namespace(management_ns, "/management")
 
 
 
--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app.register_blueprint(blueprint)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5040', debug=True)
+
+#import utils.database
+#db_handler = utils.database.DBHandler()
+
+# @app.route('/', methods=['GET'])
+# def vist():
+#     if request.method == "GET":
+#         _flag, data = db_handler.executer('''SELECT * FROM users;''')
+#         return jsonify({"dd":data})
