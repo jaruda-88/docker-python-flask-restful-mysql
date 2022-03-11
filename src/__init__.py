ffed56ffdf4932ec44f887a9c7a84103913ff594
jeongmin.lee <jaruda88@gmail.com>
2022-03-11 18:18:04 +0900

    스웨거 테스트 코드

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 
 from src.todos.todo import Todo
+from src.test.sample import sample
 
 blueprint = Blueprint("api", __name__)
 
 api = Api(blueprint)
 
 api.add_resource(Todo, '/todo')
+api.add_resource(sample, '/sample')
