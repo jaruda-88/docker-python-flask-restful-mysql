676ab7b80e9e11099ced62ef889b8235eef32342
jeongmin.lee <jaruda88@gmail.com>
2022-03-16 18:24:08 +0900

    회원가입 작업중

--- src/registration_user/reg_user.py
+++ src/registration_user/reg_user.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from unittest import result
 from flask_restful import Resource
 from flask import jsonify, request as f_request
 from flasgger import Swagger, swag_from
@@ -11,4 +12,25 @@ db = database.DBHandler()
 class RegistrationUser(Resource):
     @swag_from('reg_user.yml', validation=True)
     def post(self):
-        pass
+        try:
+            rj = f_request.get_json()
+
+            userid = rj['userid']
+            usernm = rj['username']
+            pw = rj['pw']
+
+            if userid == "":
+                raise Exception('userid is empty')
+            
+            if pw == "":
+                raise Exception('password is empty')
+
+            _flag, tb_user = db.query( '''SELECT * FROM tb_user;''' )
+
+            if _flag == False:
+                raise Exception(f"{result[0]} : {result[1]}")            
+
+            return ""
+
+        except Exception as ex:
+            return jsonify(ex.args[0])
